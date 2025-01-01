0fde5c797371eafcd96bd0e3e2a34f1642faa403
omar mohamed <omarmohamed9542@gmail.com>
2025-01-01 14:29:42 +0200

    staff data

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        staff emploee1 = new staff();
+        emploee1.name = "omar" ;
+        emploee1.salary=2000;
+        emploee1.staffId=59;
+        emploee1.printStaffData();
+        staff emploee2 = new staff();
+        emploee2.name="ali";
+        emploee2.salary=1200;
+        emploee2.staffId=26;
+        emploee2.printStaffData();
+
+
+    }
+}
---
+++ src/staff.java
@@ -0,0 +1,12 @@
+public class staff {
+
+    String name ;
+    double salary;
+    int staffId ;
+
+    public void printStaffData (){
+        System.out.println(name);
+        System.out.println(salary);
+        System.out.println(staffId);
+    }
+}
